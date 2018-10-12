e435fb6aeb53ac954887c907153a327228846333
ConnorBeam <44001088+ConnorBeam@users.noreply.github.com>
2018-10-12 10:51:42 -0700

    Add files via upload

---
+++ Knock_Lock/KeyPadLock/KeyPadLock.ino
@@ -0,0 +1,138 @@
+#include <Keypad.h>
+#include <Servo.h>
+#include <Wire.h>
+Servo myServo;
+
+const byte ROWS = 4;
+const byte COLS = 3;
+
+const int yellowLed = 2;
+const int greenLed = 3;
+const int redLed = 4;
+
+boolean locked = false;
+
+char* combo = "8923";
+String comboattempt = "";
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+byte rowPins[ROWS] = {12, 11, 10, 9};
+byte colPins[COLS] = {8, 7, 6};
+int x = 0;
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Wire.begin();
+  pinMode(yellowLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  Serial.begin(9600);
+  myServo.attach(5);
+  unlock();
+}
+
+void loop() {
+  char customKey = customKeypad.getKey();
+  if (locked == false) {
+    switch (customKey) {
+      case '*':
+        lock();
+        break;
+      default:
+        break;
+    }
+  }
+  if (locked == true) {
+    switch (customKey) {
+      case NO_KEY:
+        break;
+      case '#':
+        Serial.println ();
+        if (comboattempt == combo) {
+          Serial.print("Correct code entered: ");
+          Serial.println(comboattempt);
+          Wire.beginTransmission(9);
+          Wire.write(13);
+          Wire.endTransmission();
+          finishTransmission();
+          unlock();
+        } else {
+          Serial.print("Incorrect code entered: ");
+          Serial.println(comboattempt);
+          for (int i = 0; i < 5; i++) {
+            delay(100);
+            digitalWrite(redLed, LOW);
+            delay(100);
+            digitalWrite(redLed, HIGH);
+          }
+          Wire.beginTransmission(9);
+          Wire.write(12);
+          Wire.endTransmission();
+          finishTransmission();
+          resetCombo();
+        }
+        break;
+      case '*':
+        resetCombo();
+        break;
+      default:
+        comboattempt = comboattempt += customKey;
+        int reeee = customKey;
+        Wire.beginTransmission(9);
+        Wire.write(reeee);
+        Wire.endTransmission();
+        finishTransmission();
+        Serial.print(customKey);
+        Serial.println(" entered");
+        Serial.print("Current attempt: ");
+        Serial.println(comboattempt);
+        digitalWrite(yellowLed, HIGH);
+        delay(50);
+        digitalWrite(yellowLed, LOW);
+    }
+  }
+}
+
+//Custom Functions Below
+void unlock() {
+  delay(50);
+  Wire.beginTransmission(9);
+  Wire.write(11);
+  Wire.endTransmission();
+  finishTransmission();
+  digitalWrite(redLed, LOW);
+  digitalWrite(greenLed, HIGH);
+  Serial.println("The box is unlocked!");
+  myServo.write(0);
+  locked = false;
+}
+void lock() {
+  Wire.beginTransmission(9);
+  Wire.write(10);
+  Wire.endTransmission();
+  finishTransmission();
+  digitalWrite(greenLed, LOW);
+  Serial.println("The box is locked!");
+  myServo.write(90);
+  digitalWrite(redLed, HIGH);
+  locked = true;
+}
+void resetCombo() {
+  comboattempt = "";
+  Wire.beginTransmission(9);
+  Wire.write(14);
+  Wire.endTransmission();
+  finishTransmission();
+  Serial.println("Reset current combination attempt");
+}
+
+void finishTransmission() {
+  Wire.beginTransmission(9);
+  Wire.write(0);
+  Wire.endTransmission();
+}
+
---
+++ SerialReceiveLCD/SerialReceiveLCD.ino
@@ -0,0 +1,84 @@
+#include <LiquidCrystal.h>
+#include <Wire.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int x = 0;
+int realNumber = 0;
+String attempt = "";
+boolean locked = false;
+void setup() {
+  lcd.begin(16, 2);
+  Wire.begin(9);
+  Serial.begin(9600);
+  Wire.onReceive(receiveEvent);
+  lcd.print("Keypad Lock LCD");
+  lcd.setCursor(0, 1);
+  lcd.print("Initializing...");
+  delay(5000);
+  lcd.clear();
+  printUnlocked();
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+}
+
+void loop() {
+  switch (x) {
+    case 0:
+      break;
+    case 10:
+      Serial.println("LOCKED");
+      delay(50);
+      attempt = "";
+      printLocked();
+      break;
+    case 11:
+      Serial.println("UNLOCKED");
+      delay(50);
+      attempt = "";
+      printUnlocked();
+      break;
+    case 12:
+      Serial.println("FAILED ATTEMPT");
+      delay(50);
+      attempt = "";
+      break;
+    case 13:
+      Serial.println("CORRECT ATTEMPT");
+      delay(50);
+      attempt = "";
+      break;
+    case 14:
+      Serial.println("COMBO RESET");
+      delay(50);
+      attempt = "";
+      break;
+    default:
+      realNumber = x - 48;;
+      attempt += realNumber;
+      Serial.print("Received: ");
+      Serial.println(realNumber);
+      Serial.print("attempt: ");
+      Serial.println(attempt);
+      printLocked();
+      delay(50);
+  }
+}
+
+void printLocked() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Box is locked");
+  lcd.setCursor(0, 1);
+  lcd.print("Attempt: ");
+  lcd.setCursor(10, 1);
+  lcd.print(attempt);
+}
+void printUnlocked() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Box is unlocked");
+  lcd.setCursor(0, 1);
+  lcd.print("Press * to lock");
+}
+
